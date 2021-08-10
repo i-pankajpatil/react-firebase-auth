2cd93fbb8fe06de64281f74668c1f23b3c96f9eb
Pankaj Patil <35091748+pankajpatil2712@users.noreply.github.com>
2021-08-10 17:42:42 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,11 @@
   // Your web app's Firebase configuration
   const firebaseConfig = {
-    apiKey: "AIzaSyBAcCHWCR6YFKEBCYdTJo2ho54sWQcQnRY",
-    authDomain: "react-firebase-c1369.firebaseapp.com",
-    projectId: "react-firebase-c1369",
-    storageBucket: "react-firebase-c1369.appspot.com",
-    messagingSenderId: "61734135521",
-    appId: "1:61734135521:web:abe35be9d37d5102ac39a0"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "STORAGE_BUCKET",
+    messagingSenderId: "MESSAGING_SENDER_ID",
+    appId: "APP_ID"
   };
 
   export default firebaseConfig;
